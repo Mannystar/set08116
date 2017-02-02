c63c894fe18cb7a7a3a7bf55952f641fbd60ae70
DESKTOP-HOME\mannystar <emmantoomey@gmail.com>
2017-02-02 18:55:53 +0000

    Commit fix

--- labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
+++ labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
@@ -155,47 +155,6 @@ bool update(float delta_time) {
 	// Update the camera
 	cam.update(delta_time);
 	return true;
-=======
-  // *********************************
-  // Use keys to update transform values
-  // WSAD - movement
-  // Arrow Keys - rotation
-  // O decrease scale, P increase scale
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // *********************************
-  // Update the camera
-  cam.update(delta_time);
-  return true;
->>>>>>> e95b2889c22d40267755bedb25c7ab7d4197c9c6
 }
 
 bool render() {
